0879dbee0278fedfd179728d7fdfd7d433d2aab8
Lucas <lucas75maia@gmail.com>
2023-01-31 20:51:29 -0300

    Configurações gerais

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.scss'
+import Home from './Components/Home/Home';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
