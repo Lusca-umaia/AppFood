731c6ce809ced173114b187f7d967d39363440aa
Lucas <lucas75maia@gmail.com>
2023-02-01 19:44:43 -0300

    Adicionando o restante das dependências

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,8 +1,11 @@
 import flit from "../assets/logo.png"
 import archivetick from "../assets/archivetick.svg"
 import vector from "../assets/Vector.svg"
-
 import Lupa from '../../assets/lupa.png'
+
+
+import './style.scss'
+
 
 const NavBar: React.FC = () => {
     return (
@@ -15,10 +18,6 @@ const NavBar: React.FC = () => {
                     src={Lupa}
                 />
                 <input type="text" placeholder="Busque por restaurante" />
-                <a href="/"><img src={flit}></img></a>
-            </div>
-            <div className="search">
-                <input type="text" placeholder="Busque por restaurante"></input>
             </div>
             <div className="buttons">
                 <a href="/orders"><img src={archivetick}></img></a>
