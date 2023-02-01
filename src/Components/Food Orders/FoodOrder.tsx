41e30a3455b6341a7680b60ca6e4e9097a080f55
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2023-01-31 23:18:16 -0300

    criação da página de pedidos

---
+++ src/Components/Food Orders/FoodOrder.tsx
@@ -0,0 +1,33 @@
+import NavBar from "../NavBar/NavBar";
+
+const FoodOrder: React.FC = () => {
+    return (
+        <>
+            <NavBar></NavBar>
+            <div className="orders">
+                <div className="title">
+                    <h1>Meus Pedidos</h1>
+                </div>
+                <div className="order">
+                    <div className="orderUp">
+                        <img src="https://static-images.ifood.com.br/image/upload/t_thumbnail/logosgde/7511e4a3-65d9-4851-9c4a-e81cb1180a80/202104061944_XJh4_i.png"></img>
+                        <span>Mcdonald's- Shopping Analia Franco</span>
+                    </div>
+                    <div className="orderBottom">
+                        <img src="https://static-images.ifood.com.br/image/upload/t_low/pratos/7511e4a3-65d9-4851-9c4a-e81cb1180a80/202212270445_03jqi4o5dkmu.png"></img>
+                        <span>Big Tasty</span>
+                    </div>
+                </div>
+                <div className="order">
+
+                </div>
+                <div className="order">
+
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default FoodOrder
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -2,17 +2,21 @@ import flit from "../assets/logo.png"
 import archivetick from "../assets/archivetick.svg"
 import vector from "../assets/Vector.svg"
 
+interface INavBar {
+
+}
+
 const NavBar: React.FC = () => {
     return (
         <nav>
             <div className="logo">
-                <img src={flit}></img>
+                <a href="/"><img src={flit}></img></a>
             </div>
             <div className="search">
-                <input type="text"></input>
+                <input type="text" placeholder="Busque por restaurante"></input>
             </div>
             <div className="buttons">
-                <a><img src={archivetick}></img></a>
+                <a href="/orders"><img src={archivetick}></img></a>
                 <img src={vector}></img>
             </div>
         </nav>
