0ba897c0d10c58dca55f4b216a753926809253e9
Lucas <lucas75maia@gmail.com>
2023-02-07 21:25:46 -0300

    Ajustando responsividade e criando loading

--- src/Pages/Food Orders/Order.tsx/Order.tsx
+++ src/Pages/Food Orders/Order.tsx/Order.tsx
@@ -1,4 +1,6 @@
 import Product from "../../McDonalds/Product"
+import Loading from "../../Loading/Loading"
+
 
 interface IOrder {
     data: IOrders[],
@@ -31,7 +33,7 @@ const Order: React.FC<IOrder> = ({ data, status }) => {
                         return (
                             <div className="orderBottom">
                                 <img src={product.productUrl}></img>
-                                <span>{product.quantity} {product.name}</span>
+                                <span>{product.quantity}x {product.name}</span>
                             </div>
                         )
                     })}
@@ -42,7 +44,7 @@ const Order: React.FC<IOrder> = ({ data, status }) => {
 
     return (
         <>
-            {status === 'loading' && <div className="products">Loading...</div>}
+            {status === 'loading' && <Loading />}
             {status === 'error' && <div className="products">Error fetching products</div>}
             {status === 'success' && <div className="products">{Orders(data)}</div>}
         </>
