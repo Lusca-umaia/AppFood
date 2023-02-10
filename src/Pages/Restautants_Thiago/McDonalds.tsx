2b40407b178a0441f21ebdb3e73855f82ef87fc5
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2023-02-09 21:39:01 -0300

    arrumando a quantidade dos produtos da página de McDonald's usando useContext

--- src/Pages/Restautants_Thiago/McDonalds.tsx
+++ src/Pages/Restautants_Thiago/McDonalds.tsx
@@ -4,30 +4,33 @@ import Restaurant from "./RenderRestaurant/RenderRestaurant";
 import Product from "./Product/Product";
 import { useState } from "react";
 import OrdersModal from "./Modal/OrdersModal";
+import { StorageProvider } from "./StorageContext/StorageContext";
+
 
 const McDonalds: React.FC = () => {
     const [isModalVisible, setModalVisible] = useState(false)
 
     return (
         <>
-            <NavBar modal={() => setModalVisible(true)} product={true}></NavBar>
-            {isModalVisible ? (
-                <OrdersModal OnClose={() => setModalVisible(false)}></OrdersModal>
-            ) : null}
-            <section className="effect">
-                <div className="content">
-                    {isModalVisible ? (
-                        <div className="modalEffect">
+            <StorageProvider>
+                <NavBar modal={() => setModalVisible(true)} product={true}></NavBar>
+                {isModalVisible ? (
+                    <OrdersModal OnClose={() => setModalVisible(false)}></OrdersModal>
+                ) : null}
+                <section className="effect">
+                    <div className="content">
+                        {isModalVisible ? (
+                            <div className="modalEffect">
 
+                            </div>
+                        ) : null}
+                        <Restaurant></Restaurant>
+                        <div className="products">
+                            <Product id={1}></Product>
                         </div>
-                    ) : null}
-                    <Restaurant></Restaurant>
-                    <div className="products">
-                        <Product id={1}></Product>
                     </div>
-                </div>
-            </section>
-
+                </section>
+            </StorageProvider>
         </>
     )
 }
